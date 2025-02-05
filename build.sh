da4b4dba8a064fbbbb50c481edf5ee4fe93e9892
rootuc <rootuc@notunixchan>
2025-02-05 22:58:46 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
-# clone siyuan TODO
-# apply patch TODO
-# docker build TODO
+# clone siyuan
+git clone https://github.com/siyuan-note/siyuan.git
+cd siyuan
+
+# apply patch
+git apply ../patches/siyuan/disable-update.patch
+git apply ../patches/siyuan/mock-vip-user.patch
+git apply ../patches/siyuan/add-search-files.patch
+
+# docker build
+docker build -t a .
+
 # then manually push docker image
+echo Now run docker tag a xxxx and push
+
